9a5bfda359607e49f7e841a6413d71d082da16a1
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-14 01:48:05 +0900

    premake設定途中

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-workspace "PreMakeTest"
+workspace "Project"
 configurations
 {
     "Debug",
@@ -6,41 +6,48 @@ configurations
 }
 platforms { "x64" }
 
-project "Test"
-    location      "project"
-    targetdir ("bin/%{prj.name}/%{cfg.buildcfg}")              -- 出力ディレクトリ
-    objdir    ("bin/%{prj.name}/%{cfg.buildcfg}/intermediate") -- 中間ディレクトリ
+project "Game"
+    location      "projects"
+    targetdir (".../Bulid/%{prj.name}/%{cfg.buildcfg}")              -- 出力ディレクトリ
+    objdir    (".../Build/%{prj.name}/%{cfg.buildcfg}/intermediate") -- 中間ディレクトリ
     language      "C++"
     cppdialect    "C++20"
     kind          "ConsoleApp" -- .exe
     staticruntime "on"
     defines         { "x64" } -- プリプロセッサ
     disablewarnings { "4100", "4200" } -- C4100,C4200の警告を表示しないように
-    files           { "**.h", "**.cpp", "**.hlsl", "**.hlsli" }
-    --includedirs     { "include" }                    -- 追加のインクルードディレクトリ
+    files           { 
+      "main.cpp",
+      "Projects/GlobalVariables/*.h", "Projects/GlobalVariables/*.cpp",
+      "Projects/SceneManager/*.h", "Projects/SceneManager/*.cpp",
+      "Projects/SceneManager/SubMenu/*.h", "Projects/SceneManager/SubMenu/*.cpp",
+      "Projects/SceneManager/Fade/*.h", "Projects/SceneManager/Fade/*.cpp",
+      "Projects/SceneManager/GameScene/*.h", "Projects/SceneManager/GameScene/*.cpp",
+    }
+    links           { "Alfort\\Alfort" }                  -- 外部プロジェクトとリンク
 
-    --libdirs       { "lib" }                        -- 追加のライブラリディレクトリ(必要であれば)
-    --links         { "hoge.lib" }                   -- 追加の依存ファイル(必要であれば)
-    --links           { "sampleLib" }                  -- 外部プロジェクトとリンク
+    characterset ("Unicode")
 
-    -- 警告レベル最大
+    -- 警告レベル4
     warnings "Extra"
 
     filter "configurations:Debug"                    -- 以下Debugの設定
-      --targetsuffix  "_d"                             -- exeファイルのサフィックス(sample_d.exeのようになる)
-      defines       { "DEBUG" }
+      targetsuffix  "_debug"                             -- exeファイルのサフィックス(sample_d.exeのようになる)
+      defines       { "_DEBUG", "_CONSOLE",'%(PreprocessorDefinitions)' }
       symbols       "On"                             -- シンボル情報作成
       optimize      "OFF"                            -- 最適化無し
 
     filter "configurations:Release"                  -- 以下Releaseの設定
-    defines { "NDEBUG" }
-    optimize "Full"                                  -- 最適化
+    defines { "RELEASE" }
+    targetsuffix  "_release"
+    optimize "Speed"                                  -- 最適化
 
           -- 最新のSDKを使用する
     filter "system:windows"
     systemversion "latest"
     -- ビルド後イベント
     postbuildcommands { 'copy "$(WindowsSdkDir)bin\\$(TargetPlatformVersion)\\x64\\dxcompiler.dll" "$(TargetDir)dxcompiler.dll"' }
+    posrbuildcommands { 'copy "$(WindowsSdkDir)bin\\$(TargetPlatformVersion)\\x64\\dxil.dll" "$(TargetDir)dxil.dll"' }
 
     filter { "platforms:x64" }
     architecture "x86_64"
