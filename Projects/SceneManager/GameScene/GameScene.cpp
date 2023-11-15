9d0097baea271c5f8fe1b516fbf68cb5ff5db6d5
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-15 21:10:56 +0900

    MT4-01-05

--- Projects/SceneManager/GameScene/GameScene.cpp
+++ Projects/SceneManager/GameScene/GameScene.cpp
@@ -14,17 +14,24 @@ GameScene::GameScene() :
 void GameScene::Initialize() {
 	resultString_.SetFormat("./Resources/Font/default.spritefont");
 	resultString_.scale_ *= 0.3f;
-	Quaternion rotation = Quaternion::MakeRotateAxisAngle(Vector3{1.0f,0.4f,-0.2f}.Normalize(), 0.45f);
-	Vector3 pointY = { 2.1f, -0.9f, 1.3f };
-	Mat4x4 rotateMatrix = rotation.GetMatrix();
-	Vector3 rotateByQuaternion = pointY * rotation;
-	Vector3 rotateByMatrix = pointY * rotateMatrix;
+	Quaternion rotation0 = Quaternion::MakeRotateAxisAngle(Vector3{0.71f,0.71f,0.0f}.Normalize(), 0.3f);
+	Quaternion rotation1 = Quaternion::MakeRotateAxisAngle(Vector3{ 0.71f,0.0f,0.71f }.Normalize(), std::numbers::pi_v<float>);
+	
+	std::array<float, 5> lerpT = {
+		0.0f,0.3f,0.5f,0.7f,1.0f
+	};
 
-	resultString_ << rotation << " : rotation\n"
-		<< "rotateMatrix\n"
-		<< GetMatrixString(rotateMatrix)
-		<< rotateByQuaternion << " : rotateByQuaternion\n"
-		<< rotateByMatrix << " : rotateByMatrix";
+	std::array<Quaternion, 5> interpolate = {
+		Quaternion::Slerp(rotation0, rotation1, lerpT[0]),
+		Quaternion::Slerp(rotation0, rotation1, lerpT[1]),
+		Quaternion::Slerp(rotation0, rotation1, lerpT[2]),
+		Quaternion::Slerp(rotation0, rotation1, lerpT[3]),
+		Quaternion::Slerp(rotation0, rotation1, lerpT[4])
+	};
+
+	for (size_t i = 0llu; i < interpolate.size(); i++) {
+		resultString_ << interpolate[i] << "   : interpolate" << i << ", Slerp(q0, q1, " << lerpT[i] << "f)\n";
+	}
 }
 
 void GameScene::Finalize() {
