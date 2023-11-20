6703a4ac74b20f209c5f4c93d5bc362f512e1902
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-11-20 15:49:46 +0900

    Worldクラスのインスタンスを生成

--- Projects/main.cpp
+++ Projects/main.cpp
@@ -1,30 +1,11 @@
-#include "Engine/Engine.h"
-#include "Engine/ErrorCheck/ErrorCheck.h"
-
-#include "SceneManager/GameScene/GameScene.h"
+#include <Windows.h>
+#include <memory>
+#include "World/World.h"
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	// ライブラリ初期化
-	if (!Engine::Initialize("DirectXGame", Vector2{ 1280.0f, 720.0f })) {
-		ErrorCheck::GetInstance()->ErrorTextBox(
-			"Engine::Initialize() Failed",
-			"WinMain"
-		);
-		return -1;
-	}
-	// シーンマネージャー初期化
-	auto* const sceneManager = SceneManager::GetInstace();
-	auto scene = new GameScene{};
-	
-	sceneManager->Initialize(scene);
+	auto game = std::make_unique<World>();
 
-	// メインループ
-	sceneManager->Game(BaseScene::ID::Game);
-
-	sceneManager->Finalize();
-
-	// ライブラリ終了
-	Engine::Finalize();
+	game->Execution();
 
 	return 0;
 }
