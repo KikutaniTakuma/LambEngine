4e4c57c7fe8be5843df936ec25c8066f45a8e5a2
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-22 00:57:44 +0900

    screenoutのクリア関数とDraw関数を呼ぶように変更

--- Projects/Game/Src/Scenes/GameScene/GameScene.cpp
+++ Projects/Game/Src/Scenes/GameScene/GameScene.cpp
@@ -4,6 +4,7 @@
 #include "AudioManager/AudioManager.h"
 #include "Engine/EngineUtils/FrameInfo/FrameInfo.h"
 #include "Utils/Math/Quaternion.h"
+#include "Utils/ScreenOut/ScreenOut.h"
 #include <numbers>
 #include <format>
 
@@ -16,30 +17,6 @@ void GameScene::Initialize() {
 	camera_.pos.z = -5.0f;
 	camera_.pos.y = 1.1f;
 
-	resultString_.SetFormat("./Resources/Font/default.spritefont");
-	resultString_.scale_ *= 0.3f;
-
-	Quaternion rotation = Quaternion::MakeRotateAxisAngle(Vector3{ 0.0f,0.0f,1.0f }.Normalize(), 0.45f);
-	Quaternion rotation2 = Quaternion::MakeRotateZAxis(0.45f);
-	Vector3 pointY = { 2.1f, -0.9f, 1.3f };
-	Mat4x4 rotateMatrix = rotation.GetMatrix();
-	Mat4x4 rotateMatrix2 = rotation2.GetMatrix();
-	Vector3 rotateByQuaternion = pointY * rotation;
-	Vector3 rotateByMatrix = pointY * rotateMatrix;
-	Vector3 rotateByQuaternion2 = pointY * rotation2;
-	Vector3 rotateByMatrix2 = pointY * rotateMatrix2;
-
-	resultString_ << "\n" << 
-		rotation << " : rotation\n"
-		<< "rotateMatrix\n"
-		<< GetMatrixString(rotateMatrix)
-		<< rotateByQuaternion << " : rotateByQuaternion\n"
-		<< rotateByMatrix << " : rotateByMatrix\n"
-		<< "rotateMatrix2\n"
-		<< GetMatrixString(rotateMatrix2)
-		<< rotateByQuaternion2 << " : rotateByQuaternion\n"
-		<< rotateByMatrix2 << " : rotateByMatrix";
-
 	model_.ThreadLoadObj("./Resources/Watame/Watame.obj");
 
 	pera_.Initialize("./Resources/Shaders/PostShader/Post.VS.hlsl", "./Resources/Shaders/PostShader/PostNone.PS.hlsl");
@@ -51,7 +28,6 @@ void GameScene::Finalize() {
 }
 
 void GameScene::Update() {
-	resultString_.Debug("result");
 	model_.Update();
 	pera_.Update();
 }
@@ -59,7 +35,27 @@ void GameScene::Update() {
 void GameScene::Draw() {
 	camera_.Update(Vector3::zero);
 	pera_.PreDraw();
-	resultString_.Draw();
+
+	Quaternion rotation = Quaternion::MakeRotateAxisAngle(Vector3{ 0.0f,0.0f,1.0f }.Normalize(), 0.45f);
+	Quaternion rotation2 = Quaternion::MakeRotateZAxis(0.45f);
+	Vector3 pointY = { 2.1f, -0.9f, 1.3f };
+	Mat4x4 rotateMatrix = rotation.GetMatrix();
+	Mat4x4 rotateMatrix2 = rotation2.GetMatrix();
+	Vector3 rotateByQuaternion = pointY * rotation;
+	Vector3 rotateByMatrix = pointY * rotateMatrix;
+	Vector3 rotateByQuaternion2 = pointY * rotation2;
+	Vector3 rotateByMatrix2 = pointY * rotateMatrix2;
+
+	Lamb::screenout <<
+		rotation << " : rotation" << Lamb::endline
+		<< "rotateMatrix" << Lamb::endline
+		<< GetMatrixString(rotateMatrix)
+		<< rotateByQuaternion << " : rotateByQuaternion" << Lamb::endline
+		<< rotateByMatrix << " : rotateByMatrix" << Lamb::endline
+		<< "rotateMatrix2" << Lamb::endline
+		<< GetMatrixString(rotateMatrix2)
+		<< rotateByQuaternion2 << " : rotateByQuaternion" << Lamb::endline
+		<< rotateByMatrix2 << " : rotateByMatrix";
 
 	model_.Draw(camera_.GetViewProjection(), camera_.GetPos());
 
--- Projects/Game/Src/Scenes/GameScene/GameScene.h
+++ Projects/Game/Src/Scenes/GameScene/GameScene.h
@@ -31,7 +31,6 @@ public:
 	void Draw() override;
 
 public:
-	StringOut resultString_;
 	Model model_;
 
 	PeraRender pera_;
