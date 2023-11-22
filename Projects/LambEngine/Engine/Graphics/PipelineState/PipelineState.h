0ae5cf4fbd187790b817e3e7c99d7c97c740aa80
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-23 01:07:05 +0900

    パイプラインのみを作って使えるようにする予定のクラスヘッダーとソースファイルを追加

---
+++ Projects/LambEngine/Engine/Graphics/PipelineState/PipelineState.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+//class PipelineState {
+//public:
+//	PipelineState();
+//	PipelineState(const PipelineState&) = default;
+//	PipelineState(PipelineState&&) = default;
+//	~PipelineState();
+//
+//	PipelineState& operator=(const PipelineState&) = default;
+//	PipelineState& operator=(PipelineState&&) = default;
+//
+//public:
+//	void Create(const std::string& vertexShader, const std::string& pixelShader);
+//
+//	void Use();
+//
+//private:
+//	class Pipeline* pipeline_;
+//};
