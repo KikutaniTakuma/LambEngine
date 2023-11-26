6ff3b9b0bac523072f5bc425e0f49198cf8a4aea
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-26 21:10:36 +0900

    関数の宣言を削除

--- Projects/LambEngine/Engine/Core/ImGuiManager/ImGuiManager.cpp
+++ Projects/LambEngine/Engine/Core/ImGuiManager/ImGuiManager.cpp
@@ -11,8 +11,6 @@
 #include "imgui.h"
 #include "imgui_impl_dx12.h"
 #include "imgui_impl_win32.h"
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wPram, LPARAM lPram);
-
 
 ImGuiManager* ImGuiManager::instance_ = nullptr;
 
