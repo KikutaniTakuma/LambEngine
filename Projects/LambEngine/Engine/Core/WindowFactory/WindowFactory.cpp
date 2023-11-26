17808db2b490aaff42c9d813593956cf5adfbf5c
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-26 21:10:00 +0900

    インデントを修正

--- Projects/LambEngine/Engine/Core/WindowFactory/WindowFactory.cpp
+++ Projects/LambEngine/Engine/Core/WindowFactory/WindowFactory.cpp
@@ -2,6 +2,7 @@
 #pragma comment(lib, "winmm.lib")
 #include <cassert>
 #include "imgui_impl_win32.h"
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 extern IMGUI_IMPL_API LRESULT
 ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
