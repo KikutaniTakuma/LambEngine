85edb025a673cc0079be9c24bb93eda917d9b99e
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-12-09 01:25:55 +0900

    Engineでよく使うデータを取得するラッパー関数を定義

---
+++ Projects/LambEngine/Utils/EngineInfo/EngineInfo.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <chrono>
+#include "Utils/Math/Vector2.h"
+
+namespace Lamb {
+	float DeltaTime();
+
+	Vector2 ClientSize();
+
+	Vector2 WindowSize();
+
+	std::chrono::steady_clock::time_point ThisFrameTime();
+}
