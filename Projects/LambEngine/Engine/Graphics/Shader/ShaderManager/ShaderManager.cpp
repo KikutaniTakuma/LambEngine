88cefbc3e47e07b6168b23172818d40aa4058bd7
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-10 19:38:06 +0900

    emptyの時のif文を削除

--- Projects/LambEngine/Engine/Graphics/Shader/ShaderManager/ShaderManager.cpp
+++ Projects/LambEngine/Engine/Graphics/Shader/ShaderManager/ShaderManager.cpp
@@ -38,83 +38,48 @@ void ShaderManager::Finalize() {
 }
 
 IDxcBlob* ShaderManager::LoadVertexShader(const std::string& fileName) {
-	if (instance_->vertexShader_.empty()) {
+	auto itr = instance_->vertexShader_.find(fileName);
+	if (itr == instance_->vertexShader_.end()) {
 		IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"vs_6_0");
 		assert(shader);
 		instance_->vertexShader_.insert(std::make_pair(fileName, shader));
 	}
-	else {
-		auto itr = instance_->vertexShader_.find(fileName);
-		if (itr == instance_->vertexShader_.end()) {
-			IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"vs_6_0");
-			assert(shader);
-			instance_->vertexShader_.insert(std::make_pair(fileName, shader));
-		}
-	}
 	return instance_->vertexShader_[fileName].Get();
 }
 IDxcBlob* ShaderManager::LoadHullShader(const std::string& fileName) {
-	if (instance_->hullShader_.empty()) {
+	auto itr = instance_->hullShader_.find(fileName);
+	if (itr == instance_->hullShader_.end()) {
 		IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"hs_6_0");
 		assert(shader);
 		instance_->hullShader_.insert(std::make_pair(fileName, shader));
-	}
-	else {
-		auto itr = instance_->hullShader_.find(fileName);
-		if (itr == instance_->hullShader_.end()) {
-			IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"hs_6_0");
-			assert(shader);
-			instance_->hullShader_.insert(std::make_pair(fileName, shader));
-		}
 	}
 
 	return instance_->hullShader_[fileName].Get();
 }
 IDxcBlob* ShaderManager::LoadDomainShader(const std::string& fileName) {
-	if (instance_->domainShader_.empty()) {
+	auto itr = instance_->domainShader_.find(fileName);
+	if (itr == instance_->domainShader_.end()) {
 		IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"ds_6_0");
 		assert(shader);
 		instance_->domainShader_.insert(std::make_pair(fileName, shader));
 	}
-	else {
-		auto itr = instance_->domainShader_.find(fileName);
-		if (itr == instance_->domainShader_.end()) {
-			IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"ds_6_0");
-			assert(shader);
-			instance_->domainShader_.insert(std::make_pair(fileName, shader));
-		}
-	}
 	return instance_->domainShader_[fileName].Get();
 }
 IDxcBlob* ShaderManager::LoadGeometoryShader(const std::string& fileName) {
-	if (instance_->geometoryShader_.empty()) {
+	auto itr = instance_->geometoryShader_.find(fileName);
+	if (itr == instance_->geometoryShader_.end()) {
 		IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"gs_6_0");
 		assert(shader);
 		instance_->geometoryShader_.insert(std::make_pair(fileName, shader));
 	}
-	else {
-		auto itr = instance_->geometoryShader_.find(fileName);
-		if (itr == instance_->geometoryShader_.end()) {
-			IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"gs_6_0");
-			assert(shader);
-			instance_->geometoryShader_.insert(std::make_pair(fileName, shader));
-		}
-	}
 	return instance_->geometoryShader_[fileName].Get();
 }
 IDxcBlob* ShaderManager::LoadPixelShader(const std::string& fileName) {
-	if (instance_->pixelShader_.empty()) {
+	auto itr = instance_->pixelShader_.find(fileName);
+	if (itr == instance_->pixelShader_.end()) {
 		IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"ps_6_0");
 		assert(shader);
 		instance_->pixelShader_.insert(std::make_pair(fileName, shader));
-	}
-	else {
-		auto itr = instance_->pixelShader_.find(fileName);
-		if (itr == instance_->pixelShader_.end()) {
-			IDxcBlob* shader = instance_->shaderFactory_->CompilerShader(ConvertString(fileName), L"ps_6_0");
-			assert(shader);
-			instance_->pixelShader_.insert(std::make_pair(fileName, shader));
-		}
 	}
 	return instance_->pixelShader_[fileName].Get();
 }
