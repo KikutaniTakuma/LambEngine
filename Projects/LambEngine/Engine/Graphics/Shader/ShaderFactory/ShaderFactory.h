82c6cf449016f1067293485ac303ccc8d5a51644
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-10 19:33:46 +0900

    ShaderFactoryクラスを定義

---
+++ Projects/LambEngine/Engine/Graphics/Shader/ShaderFactory/ShaderFactory.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <d3d12.h>
+#pragma comment(lib, "d3d12.lib")
+#include <dxgi1_6.h>
+#pragma comment(lib, "dxgi.lib")
+#include <dxcapi.h>
+#pragma comment(lib, "dxcompiler.lib")
+#include <wrl.h>
+
+#include <string>
+
+class ShaderFactory {
+private:
+	ShaderFactory();
+	ShaderFactory(const ShaderFactory&) = delete;
+	ShaderFactory(ShaderFactory&&)  =delete;
+	~ShaderFactory() = default;
+
+	ShaderFactory& operator=(const ShaderFactory&) = delete;
+	ShaderFactory& operator=(ShaderFactory&&) = delete;
+
+public:
+	static void Initialize();
+
+	static void Finalize();
+
+	static ShaderFactory* const GetInstance();
+
+private:
+	static ShaderFactory* instance_;
+
+public:
+	[[nodiscard]]
+	IDxcBlob* CompilerShader(
+		// CompilerするShaderファイルへのパス
+		const std::wstring& filePath,
+		// Compilerに使用するProfile
+		const wchar_t* profile);
+
+private:
+	Microsoft::WRL::ComPtr<IDxcUtils> dxcUtils_;
+	Microsoft::WRL::ComPtr<IDxcCompiler3> dxcCompiler_;
+	Microsoft::WRL::ComPtr<IDxcIncludeHandler> includeHandler_;
+};
