70c6a7e196831f806be59ddf47da35e76b9ec7f6
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-10 19:49:11 +0900

    emptyの時のif文を削除

--- Projects/LambEngine/TextureManager/TextureManager.cpp
+++ Projects/LambEngine/TextureManager/TextureManager.cpp
@@ -98,7 +98,8 @@ TextureManager::~TextureManager() {
 
 
 Texture* const TextureManager::LoadTexture(const std::string& fileName) {
-	if (textures_.empty()) {
+	auto itr = textures_.find(fileName);
+	if (itr == textures_.end()) {
 		auto tex = std::make_unique<Texture>();
 		tex->Load(fileName, commandList_.Get());
 
@@ -138,65 +139,18 @@ Texture* const TextureManager::LoadTexture(const std::string& fileName) {
 		}
 
 		tex->srvHeapHandleUint_ = srvHeap_->CreateTxtureView(tex.get());
-		
+
 		textures_.insert(std::make_pair(fileName, std::move(tex)));
 
 		thisFrameLoadFlg_ = true;
 	}
-	else {
-		auto itr = textures_.find(fileName);
-		if (itr == textures_.end()) {
-			auto tex = std::make_unique<Texture>();
-			tex->Load(fileName, commandList_.Get());
 
-			/// コマンドリスト
-			commandList_->Close();
-			ID3D12CommandList* commandLists[] = { commandList_.Get() };
-			commandQueue_->ExecuteCommandLists(_countof(commandLists), commandLists);
-			// Fenceの値を更新
-			fenceVal_++;
-			// GPUがここまでたどり着いたときに、Fenceの値を指定した値に代入するようにSignalを送る
-			commandQueue_->Signal(fence_.Get(), fenceVal_);
-
-			// Fenceの値が指定したSigna値にたどり着いているか確認する
-			// GetCompletedValueの初期値はFence作成時に渡した初期値
-			if (fence_->GetCompletedValue() < fenceVal_) {
-				// 指定したSignal値にたどり着いていないので、たどり着くまで待つようにイベントを設定する
-				fence_->SetEventOnCompletion(fenceVal_, fenceEvent_);
-				// イベントを待つ
-				WaitForSingleObject(fenceEvent_, INFINITE);
-			}
-
-			// 次フレーム用のコマンドリストを準備
-			auto hr = commandAllocator_->Reset();
-			assert(SUCCEEDED(hr));
-			if (!SUCCEEDED(hr)) {
-				Lamb::ErrorLog("CommandAllocator somthing error", "Reset()", "TextureManager");
-			}
-			hr = commandList_->Reset(commandAllocator_.Get(), nullptr);
-			assert(SUCCEEDED(hr));
-			if (!SUCCEEDED(hr)) {
-				Lamb::ErrorLog("CommandList somthing error", "Reset()", "TextureManager");
-			}
-			///
-
-			if (!tex->isLoad_) {
-				return nullptr;
-			}
-
-			tex->srvHeapHandleUint_ = srvHeap_->CreateTxtureView(tex.get());
-
-			textures_.insert(std::make_pair(fileName, std::move(tex)));
-
-			thisFrameLoadFlg_ = true;
-		}
-	}
-	
 	return textures_[fileName].get();
 }
 
 Texture* const TextureManager::LoadTexture(const std::string& fileName, ID3D12GraphicsCommandList* const commandList) {
-	if (textures_.empty()) {
+	auto itr = textures_.find(fileName);
+	if (itr == textures_.end()) {
 		auto tex = std::make_unique<Texture>();
 		tex->Load(fileName, commandList);
 		if (!tex->isLoad_) {
@@ -208,22 +162,6 @@ Texture* const TextureManager::LoadTexture(const std::string& fileName, ID3D12Gr
 		textures_.insert(std::make_pair(fileName, std::move(tex)));
 
 		thisFrameLoadFlg_ = true;
-	}
-	else {
-		auto itr = textures_.find(fileName);
-		if (itr == textures_.end()) {
-			auto tex = std::make_unique<Texture>();
-			tex->Load(fileName, commandList);
-			if (!tex->isLoad_) {
-				return nullptr;
-			}
-
-			tex->srvHeapHandleUint_ = srvHeap_->CreateTxtureView(tex.get());
-
-			textures_.insert(std::make_pair(fileName, std::move(tex)));
-
-			thisFrameLoadFlg_ = true;
-		}
 	}
 
 	return textures_[fileName].get();
