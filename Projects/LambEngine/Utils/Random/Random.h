5a0744260dd08ac0b3f07f16acf7049ed2aaccad
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-12 12:02:21 +0900

    整数型の型をtemplate関数に変更

--- Projects/LambEngine/Utils/Random/Random.h
+++ Projects/LambEngine/Utils/Random/Random.h
@@ -1,55 +1,32 @@
 #pragma once
 
+#include <random>
 #include <cstdint>
 #include "Math/Vector2.h"
 #include "Math/Vector3.h"
 #include "Math/Vector4.h"
+#include "Utils/Cocepts/Cocepts.h"
 
 namespace Lamb {
 	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
+	/// 整数型のランダム関数
 	/// </summary>
+	/// <typeparam name="T">32bit以下の整数型のみサポート</typeparam>
 	/// <param name="min">最小値</param>
 	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	int8_t Random(int8_t min, int8_t max);
-	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
-	/// </summary>
-	/// <param name="min">最小値</param>
-	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	uint8_t Random(uint8_t min, uint8_t max);
+	/// <returns>ランダムな値</returns>
+	template<IsIntSmallerThan32bit T>
+	T Random(T min, T max) {
+		static std::random_device seed;
+		static std::mt19937_64 rnd(seed());
+		if (max < min) {
+			std::swap(min, max);
+		}
 
-	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
-	/// </summary>
-	/// <param name="min">最小値</param>
-	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	int16_t Random(int16_t min, int16_t max);
-	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
-	/// </summary>
-	/// <param name="min">最小値</param>
-	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	uint16_t Random(uint16_t min, uint16_t max);
+		std::uniform_int_distribution<T> dist{ min, max };
 
-	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
-	/// </summary>
-	/// <param name="min">最小値</param>
-	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	int32_t Random(int32_t min, int32_t max);
-	/// <summary>
-	/// 最小値から最大値までで一様分布で乱数を生成
-	/// </summary>
-	/// <param name="min">最小値</param>
-	/// <param name="max">最大値</param>
-	/// <returns>乱数</returns>
-	uint32_t Random(uint32_t min, uint32_t max);
+		return static_cast<T>(dist(rnd));
+	}
 
 	/// <summary>
 	/// 最小値から最大値までで一様分布で乱数を生成
