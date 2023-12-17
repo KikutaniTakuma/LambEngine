46acfe8d0b59e480777d6d1564129f07d65d423c
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-17 14:30:28 +0900

    try,catchをFrameworkクラスに移行

--- Projects/Game/Src/main.cpp
+++ Projects/Game/Src/main.cpp
@@ -1,18 +1,11 @@
 #include <Windows.h>
 #include <memory>
 #include "World/World.h"
-#include "Utils/ExecutionLog/ExecutionLog.h"
-#include "Error/Error.h"
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	try {
-		auto game = std::make_unique<World>();
+	auto game = std::make_unique<World>();
 
-		game->Execution();
-	}
-	catch (const Lamb::Error& err) {
-		Lamb::ErrorLog(err);
-	}
+	game->Execution();
 
 	return 0;
 }
