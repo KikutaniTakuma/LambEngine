d53c2eb9251ae1b354305bfcbd45d3b3f9dace20
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-17 14:24:42 +0900

    namespaceをErrorクラスにつけた

--- Projects/LambEngine/Error/Error.h
+++ Projects/LambEngine/Error/Error.h
@@ -3,45 +3,47 @@
 #include <typeinfo>
 #include "Utils/ExecutionLog/ExecutionLog.h"
 
-class Error {
-public:
-	Error() = default;
-	Error(const Error&) = default;
-	Error(Error&&) = default;
-	~Error() = default;
+namespace Lamb {
+	class Error {
+	public:
+		Error() = default;
+		Error(const Error&) = default;
+		Error(Error&&) = default;
+		~Error() = default;
 
-	Error& operator=(const Error&) = default;
-	Error& operator=(Error&&) = default;
+		Error& operator=(const Error&) = default;
+		Error& operator=(Error&&) = default;
 
-public:
-	const std::string& What() const {
-		return errorCode_;
-	}
+	public:
+		const std::string& What() const {
+			return errorCode_;
+		}
 
-	const std::string& FunctionName() const {
-		return functionName_;
-	}
+		const std::string& FunctionName() const {
+			return functionName_;
+		}
 
-	const std::string& ClassName() const {
-		return className_;
-	}
+		const std::string& ClassName() const {
+			return className_;
+		}
 
-private:
-	std::string className_;
-	std::string errorCode_;
-	std::string functionName_;
+	private:
+		std::string className_;
+		std::string errorCode_;
+		std::string functionName_;
 
-public:
-	template<class T>
-	static const Error& Code(const std::string& errorCode, const std::string& functionName) {
-		static Error err;
-		err = Error{};
+	public:
+		template<class T>
+		static const Error& Code(const std::string& errorCode, const std::string& functionName) {
+			static Error err;
+			err = Error{};
 
-		err.errorCode_ = errorCode;
-		err.className_ = typeid(T).name();
-		err.functionName_ = functionName;
-		Lamb::DebugLog(std::string{ typeid(Error).name() } + " " + err.ClassName() + " : " + err.FunctionName() + " : " + err.What());
+			err.errorCode_ = errorCode;
+			err.className_ = typeid(T).name();
+			err.functionName_ = functionName;
+			Lamb::DebugLog(std::string{ typeid(Error).name() } + " " + err.ClassName() + " : " + err.FunctionName() + " : " + err.What());
 
-		return err;
-	}
-};
+			return err;
+		}
+	};
+}
