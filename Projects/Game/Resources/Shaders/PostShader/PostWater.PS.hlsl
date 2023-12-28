5a07a10cef4ef2002d68ccfec27affcc445b5f9d
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-12-28 23:04:20 +0900

    水面っぽくなった

--- Projects/Game/Resources/Shaders/PostShader/PostWater.PS.hlsl
+++ Projects/Game/Resources/Shaders/PostShader/PostWater.PS.hlsl
@@ -39,7 +39,7 @@ float Perlin(float density, float2 uv)
     float v0010 = lerp(c00, c10, u.x);
     float v0111 = lerp(c01, c11, u.x);
 
-    float n = lerp(v0010, v0111, u.y) / 2.0f + 0.5f;
+    float n = (lerp(v0010, v0111, u.y) * 0.5f) + 0.5f;
     
     return n;
 }
@@ -57,26 +57,30 @@ float FractalSumNnoise(float density, float2 uv)
 
 float CreateNoise(float2 uv)
 {
-    float pn = FractalSumNnoise(10.0f, uv + randomVec);
-    float pn2 = FractalSumNnoise(5.0f, uv - randomVec);
+    float density = 5.0f;
+    
+    float pn = FractalSumNnoise(density, uv + randomVec);
+    float pn2 = FractalSumNnoise(density, uv - randomVec);
     uv.x *= -1.0f;
-    float pn3 = FractalSumNnoise(10.0f, uv + randomVec);
+    float pn3 = FractalSumNnoise(density, uv + randomVec);
     uv.x *= -1.0f;
     uv.y *= -1.0f;
-    float pn4 = FractalSumNnoise(5.0f, uv + randomVec);
+    float pn4 = FractalSumNnoise(density, uv + randomVec);
     
-    float noise = lerp((pn * 0.1f), (pn2 * 0.08f), 3.0f);
-    float noise2 = lerp((pn3 * 0.1f), (pn4 * 0.08f), 3.0f);
+    float noise = lerp((pn * 0.1f), (pn2 * 0.1f), 3.0f);
+    float noise2 = lerp((pn3 * 0.1f), (pn4 * 0.1f), 3.0f);
     
-    return lerp(noise,noise2, 0.5f);
+    return ddy(noise + noise2);
 }
 
 float3 CreateNormal(float2 uv)
 {
-    float right = CreateNoise(float2(uv.x + 1.0f, uv.y));
-    float left = CreateNoise(float2(uv.x - 1.0f, uv.y));
-    float up = CreateNoise(float2(uv.x, uv.y + 1.0f));
-    float bottom = CreateNoise(float2(uv.x, uv.y - 1.0f));
+    float heightScale = 5.0f;
+    
+    float right = CreateNoise(float2(uv.x + 1.0f, uv.y)) * heightScale;
+    float left = CreateNoise(float2(uv.x - 1.0f, uv.y)) * heightScale;
+    float up = CreateNoise(float2(uv.x, uv.y + 1.0f)) * heightScale;
+    float bottom = CreateNoise(float2(uv.x, uv.y - 1.0f)) * heightScale;
     
     float dfx = right - left;
     float dfy = up - bottom;
@@ -84,7 +88,7 @@ float3 CreateNormal(float2 uv)
     float3 n = normalize(float3(-dfx, -dfy, 2.0f));
     
     float a = 0.01f;
-    n = (n / a * 1.0f) * 0.5f;
+    n = (n / a) * 0.5f;
     
     n = (n * 2.0f) - 1.0f;
    
@@ -94,7 +98,7 @@ float3 CreateNormal(float2 uv)
 float4 main(Output input) : SV_TARGET{
     float noise = CreateNoise(input.uv);
     
-    float4 texColor = tex.Sample(smp, input.uv + noise);
+    float4 texColor = tex.Sample(smp, input.uv.xy + frac(noise));
     
     
     float3 normal = CreateNormal(input.uv);
@@ -128,17 +132,18 @@ float4 main(Output input) : SV_TARGET{
     t = pow(saturate(t), 20.0f);
     float3 specDirection = light.ligColor * t;
     
-    
-    
     float3 lig = diffDirection + specDirection;
+    //float3 lig = specPerlin;
     
     lig.xyz += 0.3f;
     
-    lig = pow(lig, 1.3f);
+    lig = pow(lig, 1.5f);
     
     texColor *= color;
     texColor.xyz *= lig;
 
     return texColor;
     //return float4(normal, 1.0f);
+    
+    //return float4(float3(noise, noise, noise) * 2.0f, 1.0f);
 }
