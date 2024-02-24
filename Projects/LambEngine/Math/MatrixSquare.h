cfd33e734b4cfa2be2ba84af772279a528df900d
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-02-25 02:26:37 +0900

    正方行列クラスを定義

---
+++ Projects/LambEngine/Math/MatrixSquare.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Matrix.h"
+
+template<std::floating_point type, size_t size_>
+class MatrixSquare : public Matrix<type, size_>{
+public:
+	const MatrixSquare& identity() {
+
+	}
+
+	MatrixSquare inverse() {
+
+	}
+};
