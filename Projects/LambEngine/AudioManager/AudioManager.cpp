a54aa068bbbb44ea56c74cb159e1aecc4b11a487
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-04-23 17:57:10 +0900

    非同期読み込みを削除

--- Projects/LambEngine/AudioManager/AudioManager.cpp
+++ Projects/LambEngine/AudioManager/AudioManager.cpp
@@ -18,11 +18,7 @@ void AudioManager::Finalize() {
 AudioManager::AudioManager() :
 	xAudio2_(),
 	masterVoice_(nullptr),
-	audios_{},
-	threadAudioBuff_{},
-	load_{},
-	mtx_{},
-	isThreadLoadFinish_{false}
+	audios_{}
 {
 	// Media Foundationの初期化
 	MFStartup(MF_VERSION, MFSTARTUP_NOSOCKET);
@@ -45,9 +41,6 @@ AudioManager::AudioManager() :
 AudioManager::~AudioManager() {
 	xAudio2_.Reset();
 	MFShutdown();
-	if (load_.joinable()) {
-		load_.join();
-	}
 }
 
 Audio* const AudioManager::LoadWav(const std::string& fileName) {
@@ -69,11 +62,6 @@ Audio* const AudioManager::LoadWav(const std::string& fileName) {
 	return audios_[fileName].get();
 }
 
-void AudioManager::LoadWav(const std::string& fileName, class Audio** const audio) {
-	// コンテナに追加
-	threadAudioBuff_.push({fileName, audio});
-}
-
 void AudioManager::Unload(const std::string& fileName)
 {
 	auto isExisit = audios_.find(fileName);
@@ -93,51 +81,5 @@ void AudioManager::Unload(Audio* audio)
 
 	if (isExisit != audios_.end()) {
 		audios_.erase(isExisit);
-	}
-}
-
-void AudioManager::ThreadLoad() {
-	if (!threadAudioBuff_.empty() && !load_.joinable()) {
-		auto loadProc = [this]() {
-			std::lock_guard<std::mutex> lock(mtx_);
-			while (!threadAudioBuff_.empty()) {
-				if (Lamb::IsEngineFianlize()) {
-					break;
-				}
-
-				auto& front = threadAudioBuff_.front();
-
-				auto audio = audios_.find(front.fileName_);
-				if (audio == audios_.end()) {;
-					audios_.insert(std::make_pair(front.fileName_, std::make_unique<Audio>()));
-					audios_[front.fileName_]->Load(front.fileName_);
-					*front.audio_ = audios_[front.fileName_].get();
-
-					ResourceManager::GetInstance()->SetAudioResource(front.fileName_);
-				}
-				else {
-					*front.audio_ = audio->second.get();
-				}
-
-				threadAudioBuff_.pop();
-			}
-
-			isThreadLoadFinish_ = true;
-			};
-
-		load_ = std::thread{ loadProc };
-	}
-}
-
-void AudioManager::JoinThread() {
-	if (load_.joinable() && threadAudioBuff_.empty()) {
-		load_.join();
-	}
-}
-
-void AudioManager::CheckThreadLoadFinish() {
-	if (isThreadLoadFinish_ && threadAudioBuff_.empty()) {
-		JoinThread();
-		isThreadLoadFinish_ = false;
 	}
 }
