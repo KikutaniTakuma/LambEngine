5c361f726b925c31ad95ec80c37b882962ba1c7b
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-04-24 17:01:59 +0900

    水板ポリ用クラスの定義(まだただの板ポリ)

---
+++ Projects/LambEngine/Drawers/Other/WaterTex2D/WaterTex2D.cpp
@@ -0,0 +1,54 @@
+#include "WaterTex2D.h"
+#include "Engine/Graphics/TextureManager/TextureManager.h"
+#include "Engine/Graphics/RenderContextManager/RenderContextManager.h"
+#include "Utils/SafePtr/SafePtr.h"
+
+#ifdef _DEBUG
+#include "Utils/UtilsLib/UtilsLib.h"
+
+#include "imgui.h"
+#endif // _DEBUG
+
+
+const LoadFileNames WaterTex2D::kFileNames_ =
+LoadFileNames{
+	.resourceFileName{"./Resources/EngineResources/WaterTex2D/WaterTex2D.obj"},
+	.shaderName{
+		.vsFileName = "./Resources/Shaders/Texture2DShader/WaterTex2D.VS.hlsl",
+		.psFileName = "./Resources/Shaders/Texture2DShader/WaterTex2D.PS.hlsl"
+	}
+};
+
+WaterTex2D::WaterTex2D() :
+	BaseDrawer()
+{}
+
+void WaterTex2D::Load()
+{
+	Lamb::SafePtr renderContextManager = RenderContextManager::GetInstance();
+
+	renderContextManager->Load<WaterRenderContext>(kFileNames_);
+
+	renderSet = renderContextManager->Get(kFileNames_);
+}
+
+void WaterTex2D::Draw(
+	const Mat4x4& worldMatrix,
+	const Mat4x4& uvTransform,
+	const Mat4x4& camera,
+	uint32_t textureID,
+	uint32_t color,
+	BlendType blend
+) {
+	Lamb::SafePtr renderContext = renderSet->GetRenderContextDowncast<WaterRenderContext>(blend);
+
+	renderContext->SetSahderStruct(
+		ShaderData{
+			.uvTransform = uvTransform,
+			.pad = Vector3::kZero,
+			.textureID = textureID
+		}
+	);
+
+	BaseDrawer::Draw(worldMatrix, camera, color, blend);
+}
---
+++ Projects/LambEngine/Drawers/Other/WaterTex2D/WaterTex2D.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#pragma once
+#include "../../BaseDrawer.h"
+#include "Engine/Graphics/RenderContextManager/RenderContext/RenderContext.h"
+
+/// <summary>
+/// 板ポリ描画
+/// </summary>
+class WaterTex2D : public BaseDrawer {
+public:
+	static constexpr uint32_t kMaxDrawCount = 1024u;
+
+private:
+	struct ShaderData {
+		Mat4x4 uvTransform;
+		Vector3 pad; // <- huh?
+		uint32_t textureID = 0u;
+	};
+
+	using WaterRenderContext = RenderContext<ShaderData, kMaxDrawCount>;
+
+	static const LoadFileNames kFileNames_;
+
+public:
+	WaterTex2D();
+	WaterTex2D(const WaterTex2D&) = default;
+	WaterTex2D(WaterTex2D&&) = default;
+	~WaterTex2D() = default;
+
+	WaterTex2D& operator=(const WaterTex2D&) = default;
+	WaterTex2D& operator=(WaterTex2D&&) = default;
+
+public:
+	void Load();
+
+	void Draw(
+		const Mat4x4& worldMatrix,
+		const Mat4x4& uvTransform,
+		const Mat4x4& camera,
+		uint32_t textureID,
+		uint32_t color,
+		BlendType blend
+	);
+};
