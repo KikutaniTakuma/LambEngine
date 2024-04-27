905974ae3e579cb3297c500dc2d9c3951660cd70
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-04-28 00:12:37 +0900

    一旦コミット

--- Projects/Game/Src/Game/SkyBlock/SkyBlock.cpp
+++ Projects/Game/Src/Game/SkyBlock/SkyBlock.cpp
@@ -1,8 +1,14 @@
 #include "SkyBlock.h"
 #include "Camera/Camera.h"
 
+#include "Drawers/DrawerManager.h"
+
 void SkyBlock::Init(const Transform& transform) {
 	transform_ = transform;
+
+	Lamb::SafePtr draweManager = DrawerManager::GetInstance();
+	draweManager->LoadModel("./Resources/Common/Cube.obj");
+	model_ = draweManager->GetModel("./Resources/Common/Cube.obj");
 }
 
 void SkyBlock::Update() {
