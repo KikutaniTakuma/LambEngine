a52554a90be98fd8491176f4ae27851cc6ea94fe
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-09 17:37:30 +0900

    テスト

--- Projects/Game/Src/Scenes/GameScene/GameScene.cpp
+++ Projects/Game/Src/Scenes/GameScene/GameScene.cpp
@@ -12,7 +12,7 @@ void GameScene::Initialize() {
 	/*drawerManager_->LoadModel("./Resources/Common/Hololive/Watame/Watame.obj");
 	model2_ = drawerManager_->GetModel("./Resources/Common/Hololive/Watame/Watame.obj");*/
 	model_ = std::make_unique<AnimationModel>();
-	model_->Load("./Resources/Common/human/walk.gltf");
+	model_->Load("./Resources/Common/human/sneakWalk.gltf");
 	model_->GetAnimator().SetLoopAnimation(true);
 	model_->GetAnimator().Start();
 }
