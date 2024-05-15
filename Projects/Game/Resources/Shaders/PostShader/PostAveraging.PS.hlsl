446fca54a98a4a776a4719b89a6e01c0b2c7751f
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-15 15:20:47 +0900

    改良

--- Projects/Game/Resources/Shaders/PostShader/PostAveraging.PS.hlsl
+++ Projects/Game/Resources/Shaders/PostShader/PostAveraging.PS.hlsl
@@ -1,47 +1,34 @@
 #include "Post.hlsli"
 
+static const float32_t2 kIndex[5][5] = {
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f,  0.0f }, { -1.0f,  0.0f }, { 0.0f,  0.0f }, { 1.0f,  0.0f }, { 2.0f,  0.0f } },
+    { { -2.0f,  1.0f }, { -1.0f,  1.0f }, { 0.0f,  1.0f }, { 1.0f,  1.0f }, { 2.0f,  1.0f } },
+    { { -2.0f,  2.0f }, { -1.0f,  2.0f }, { 0.0f,  2.0f }, { 1.0f,  2.0f }, { 2.0f,  2.0f } },
+};
+
+static const float32_t kKernel[5][5] = {
+    { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+    { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+    { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+    { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+    { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+};
+
 float32_t4 main(Output input) : SV_TARGET{
     float32_t w, h, levels;
     tex.GetDimensions(0, w,h,levels);
 
-    float32_t dx = 1.0f / w;
-    float32_t dy = 1.0f / h;
+    float32_t dx = rcp(w);
+    float32_t dy = rcp(h);
     float32_t4 ret = float32_t4(0.0f,0.0f,0.0f,0.0f);
 
-    // 最上段
-    ret += tex.Sample(smp, input.uv + float32_t2(-2.0f * dx, 2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2(-1.0f * dx, 2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 0.0f * dx, 2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 1.0f * dx, 2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 2.0f * dx, 2.0f * dy));
+    for(uint32_t y = 0; y < 5; y++){
+        for(uint32_t x = 0; x < 5; x++){
+            ret += tex.Sample(smp, input.uv + kIndex[y][x]) * kKernel[y][x];
+        }
+    }
 
-    // 一つ上
-    ret += tex.Sample(smp, input.uv + float32_t2(-2.0f * dx, 1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2(-1.0f * dx, 1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 0.0f * dx, 1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 1.0f * dx, 1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 2.0f * dx, 1.0f * dy));
-
-    // 真ん中
-    ret += tex.Sample(smp, input.uv + float32_t2(-2.0f * dx, 0.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2(-1.0f * dx, 0.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 0.0f * dx, 0.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 1.0f * dx, 0.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 2.0f * dx, 0.0f * dy));
-
-     // 一つ下
-    ret += tex.Sample(smp, input.uv + float32_t2(-2.0f * dx, -1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2(-1.0f * dx, -1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 0.0f * dx, -1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 1.0f * dx, -1.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 2.0f * dx, -1.0f * dy));
-    
-     // 最下段
-    ret += tex.Sample(smp, input.uv + float32_t2(-2.0f * dx, -2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2(-1.0f * dx, -2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 0.0f * dx, -2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 1.0f * dx, -2.0f * dy));
-    ret += tex.Sample(smp, input.uv + float32_t2( 2.0f * dx, -2.0f * dy));
-
-    return ret / 25.0f;
+    return ret * rcp(5.0f * 5.0f);
 }
