1eb302a3cbdd190525a52ecc746abc352644f2ff
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-07-23 12:34:23 +0900

    hsv変換を削除

--- Projects/Game/Resources/Shaders/PostShader/PostNone2.PS.hlsl
+++ Projects/Game/Resources/Shaders/PostShader/PostNone2.PS.hlsl
@@ -1,13 +1,9 @@
 #include "Post.hlsli"
 #include "../OutputStructs.hlsli"
-#include "../HSV.hlsli"
 
 PixelShaderOutPut2 main(Output input) {
     PixelShaderOutPut2 output;
     output.color0 =  tex.Sample(smp, input.uv);
-    float32_t3 hsv = RGBToHSV(output.color0.rgb);
-    output.color0.rgb = HSVToRGB(hsv);
-
     output.color1 = output.color0;
 
     return output;
