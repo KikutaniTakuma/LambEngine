94dc228a1be418e348185db9c7f1bb65900825f9
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-07-25 00:13:30 +0900

    クラスだけ追加

---
+++ Projects/Game/Src/GameObject/GameComp/PlayerComp.cpp
@@ -0,0 +1,33 @@
+#include "PlayerComp.h"
+
+void PlayerComp::Init()
+{
+}
+
+void PlayerComp::Finalize()
+{
+}
+
+void PlayerComp::FirstUpdate()
+{
+}
+
+void PlayerComp::Move()
+{
+}
+
+void PlayerComp::Event()
+{
+}
+
+void PlayerComp::Update()
+{
+}
+
+void PlayerComp::LastUpdate()
+{
+}
+
+void PlayerComp::Draw()
+{
+}
--- Projects/Game/Src/GameObject/GameComp/PlayerComp.h
+++ Projects/Game/Src/GameObject/GameComp/PlayerComp.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "../Object.h"
+class PlayerComp : public IComp {
+public:
+	using IComp::IComp;
+
+	~PlayerComp() = default;
+
+private:
+	void Init() override;
+	void Finalize() override;
+
+
+	void FirstUpdate() override;
+	void Move() override;
+	void Event() override;
+	void Update() override;
+	void LastUpdate() override;
+
+	void Draw() override;
+};
