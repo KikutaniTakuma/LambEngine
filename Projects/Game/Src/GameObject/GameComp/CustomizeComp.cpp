7ea2be081a5307f24617338e71f8aa252df400c0
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-08-04 13:58:20 +0900

    再現は8月中に行う

--- Projects/Game/Src/GameObject/GameComp/CustomizeComp.cpp
+++ Projects/Game/Src/GameObject/GameComp/CustomizeComp.cpp
@@ -1,6 +1,9 @@
 #include "CustomizeComp.h"
 #include "../Comp/CsvDataComp.h"
+#include "../Comp/JsonComp.h"
 #include "../Comp/ChildrenObjectComp.h"
+#include "../Comp/ModelRenderComp.h"
+#include "../Comp/ModelRenderDataComp.h"
 
 void CustomizeComp::Save(nlohmann::json& json) {
 	SaveCompName(json);
@@ -10,11 +13,42 @@ void CustomizeComp::Load([[maybe_unused]]nlohmann::json& json) {
 
 }
 
+void CustomizeComp::Load()
+{
+	csvData_->Load();
+	jsonData_->Load();
+	auto& jsonData = jsonData_->GetJsonData();
+
+	maxCannonNum_ = jsonData["Cannon"].get<int32_t>();
+	maxLoopCannonNum_ = jsonData["LoopCannon"].get<int32_t>();
+	maxSailNum_ = jsonData["Sail"].get<int32_t>();
+
+	cannonComps_.reserve(maxCannonNum_);
+	loopCannonComps_.reserve(maxLoopCannonNum_);
+	sailComps_.reserve(maxSailNum_);
+
+	items_->ClearObjects();
+
+	/*for (auto& y : csvData_->GetCsvData()) {
+		for (auto& x : y) {
+			if (x == 1) {
+				Lamb::SafePtr addObject = Lamb::MakeSafePtr<Object>();
+				Lamb::SafePtr box = addObject->AddComp<ModelRenderComp>();
+				Lamb::SafePtr dataComp = addObject->GetComp<ModelRenderDataComp>();
+				dataComp->fileName = "./"
+				
+				items_->AddObject();
+			}
+		}
+	}*/
+}
+
 void CustomizeComp::Init() {
 	items_ = object_.AddComp<ChildrenObjectComp>();
 	csvData_ = object_.AddComp<CsvDataComp>();
+	jsonData_ = object_.AddComp<JsonComp>();
 }
 
-void CustomizeComp::Update()
-{
+void CustomizeComp::Update() {
+
 }
--- Projects/Game/Src/GameObject/GameComp/CustomizeComp.h
+++ Projects/Game/Src/GameObject/GameComp/CustomizeComp.h
@@ -1,4 +1,8 @@
 #include "../Object.h"
+#include "CannonComp.h"
+#include "LoopCannonComp.h"
+#include "SailComp.h"
+
 class CustomizeComp : public IComp {
 public:
 	using IComp::IComp;
@@ -8,6 +12,7 @@ public:
 public:
 	void Save(nlohmann::json& json) override;
 	void Load(nlohmann::json& json) override;
+	void Load() override;
 
 public:
 	void Init() override;
@@ -17,4 +22,16 @@ public:
 private:
 	Lamb::SafePtr<class ChildrenObjectComp> items_;
 	Lamb::SafePtr<class CsvDataComp> csvData_;
+	Lamb::SafePtr<class JsonComp> jsonData_;
+
+	std::vector<Lamb::SafePtr<CannonComp>> cannonComps_;
+	std::vector<Lamb::SafePtr<LoopCannonComp>> loopCannonComps_;
+	std::vector<Lamb::SafePtr<SailComp>> sailComps_;
+
+	// 大砲の数
+	int32_t maxCannonNum_ = 0;
+	// ループ大砲の数
+	int32_t maxLoopCannonNum_ = 0;
+	// 帆の数
+	int32_t maxSailNum_ = 0;
 };
