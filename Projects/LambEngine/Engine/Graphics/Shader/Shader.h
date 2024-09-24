91d8123711640db415b54437e3d6669e8830b2e4
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-09-24 12:26:55 +0900

    MeshShaderをまとめた構造体を追加

--- Projects/LambEngine/Engine/Graphics/Shader/Shader.h
+++ Projects/LambEngine/Engine/Graphics/Shader/Shader.h
@@ -12,3 +12,14 @@ struct Shader {
 
 	[[nodiscard]] bool operator==(const Shader& right) const = default;
 };
+
+/// <summary>
+/// シェーダーをまとめる構造体
+/// </summary>
+struct MeshShader {
+	struct IDxcBlob* amplification = nullptr; // 増幅シェーダー
+	struct IDxcBlob* mesh = nullptr;          // メッシュシェーダー
+	struct IDxcBlob* pixel = nullptr;         // ピクセルシェーダー
+
+	[[nodiscard]] bool operator==(const MeshShader& right) const = default;
+};
