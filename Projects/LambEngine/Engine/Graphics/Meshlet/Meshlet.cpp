e3f0e891835041585770458ebdc3df11daa93703
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-10-01 17:00:01 +0900

    meshletの作り方を変更

--- Projects/LambEngine/Engine/Graphics/Meshlet/Meshlet.cpp
+++ Projects/LambEngine/Engine/Graphics/Meshlet/Meshlet.cpp
@@ -2,7 +2,7 @@
 
 #include "../VertexIndexDataManager/VertexIndexDataManager.h"
 
-#include "meshoptimizer.h"
+#include "DirectXMesh/DirectXMesh.h"
 
 #include "Engine/Core/DescriptorHeap/CbvSrvUavHeap.h"
 
@@ -40,152 +40,59 @@ void MeshLoader::ParseMesh(ResMesh& dstMesh, Lamb::SafePtr<const ModelData> pSrc
 	// インデックスデータセット
 	dstMesh.indices = pSrcMesh->indices;
 
-	// 最適化
-	{
-		std::vector<uint32_t> remap(dstMesh.indices.size());
+	// メッシュレット作成用
+	std::vector<uint32_t> indices;
+	std::vector<DirectX::XMFLOAT3> positions;
 
-		auto vertexCount = meshopt_generateVertexRemap(
-			remap.data(),
-			dstMesh.indices.data(),
-			dstMesh.indices.size(),
-			dstMesh.vertices.data(),
-			dstMesh.vertices.size(),
-			sizeof(decltype(pSrcMesh->vertices)::value_type)
-		);
+	indices.resize(dstMesh.indices.size());
+	positions.resize(dstMesh.vertices.size());
 
-		std::vector<Vertex> vertices(vertexCount);
-		std::vector<uint32_t> indices(dstMesh.indices.size());
-
-		// 頂点インデックスを再マッピング
-		meshopt_remapIndexBuffer(
-			indices.data(),
-			dstMesh.indices.data(),
-			dstMesh.indices.size(),
-			remap.data()
-		);
-
-		// 頂点データを再マッピング
-		meshopt_remapVertexBuffer(
-			vertices.data(),
-			dstMesh.vertices.data(),
-			dstMesh.vertices.size(),
-			sizeof(decltype(pSrcMesh->vertices)::value_type),
-			remap.data()
-		);
-
-		
-		// メモリクリア
-		remap.clear();
-		remap.shrink_to_fit();
-
-		dstMesh.vertices.resize(vertices.size());
-		dstMesh.indices.resize(indices.size());
-
-		// 頂点キャッシュ最適化
-		meshopt_optimizeVertexCache(
-			dstMesh.indices.data(),
-			indices.data(),
-			indices.size(),
-			vertexCount
-		);
-
-		// メモリクリア
-		indices.clear();
-		indices.shrink_to_fit();
-
-		
-		// 頂点フェッチ最適化
-		meshopt_optimizeVertexFetch(
-			dstMesh.vertices.data(),
-			dstMesh.indices.data(),
-			dstMesh.indices.size(),
-			vertices.data(),
-			vertices.size(),
-			sizeof(decltype(pSrcMesh->vertices)::value_type)
-		);
-
-		// メモリクリア
-		vertices.clear();
-		vertices.shrink_to_fit();
+	// データ代入
+	std::copy(dstMesh.indices.cbegin(), dstMesh.indices.cend(), indices.begin());
+	for (size_t count = 0; const auto & vertex : dstMesh.vertices) {
+		SetVector3ToDirectXXMFLOAT3(positions[count], vertex.pos.GetVector3());
+		count++;
 	}
 
-	// メッシュレット生成
-	{
-		constexpr size_t kMaxVertices = 64;
-		constexpr size_t kMaxPrimitives = 128;
+	// 出力変数
+	std::vector<DirectX::Meshlet> meshlets;
+	std::vector<uint8_t> uniqueVertexIB;
+	std::vector<DirectX::MeshletTriangle> primitiveIndices;
 
-		size_t maxMeshlets = meshopt_buildMeshletsBound(
-			dstMesh.indices.size(),
-			kMaxVertices,
-			kMaxPrimitives
-		);
+	// メッシュレット作成
+	HRESULT hr = DirectX::ComputeMeshlets(
+		indices.data(), indices.size() / 3,
+		positions.data(), positions.size(),
+		nullptr,
+		meshlets,
+		uniqueVertexIB,
+		primitiveIndices
+	);
 
-		std::vector<meshopt_Meshlet> meshlets(maxMeshlets);
-
-		dstMesh.uniqueVertexIndices.resize(maxMeshlets * kMaxVertices);
-		dstMesh.primitiveIndices.resize(maxMeshlets * kMaxVertices * 3);
-
-		size_t meshletCount = meshopt_buildMeshlets(
-			meshlets.data(), 
-			dstMesh.uniqueVertexIndices.data(),
-			dstMesh.primitiveIndices.data(),
-			dstMesh.indices.data(),
-			dstMesh.indices.size(),
-			dstMesh.vertices.front().pos.data(),
-			dstMesh.vertices.size(),
-			sizeof(Vertex), 
-			kMaxVertices,
-			kMaxPrimitives,
-			0.0f
-		);
-
-		meshlets.resize(meshletCount);
-
-		const meshopt_Meshlet& last = meshlets.back();
-
-		dstMesh.uniqueVertexIndices.resize(last.vertex_offset + last.vertex_count);
-		dstMesh.primitiveIndices.resize(last.triangle_offset + ((last.triangle_count * 3 + 3) & ~3));
-		meshlets.resize(meshletCount);
-
-		dstMesh.uniqueVertexIndices.shrink_to_fit();
-		dstMesh.primitiveIndices.shrink_to_fit();
-
-		for (auto& m : meshlets) {
-			meshopt_optimizeMeshlet(
-				&dstMesh.uniqueVertexIndices[m.vertex_offset],
-				&dstMesh.primitiveIndices[m.triangle_offset],
-				m.triangle_count,
-				m.vertex_count
-			);
-		}
-
-		dstMesh.meshlets.resize(meshlets.size());
-		uint32_t primitiveCount = 0;
-
-		for (size_t i = 0; i < meshlets.size(); ++i) {
-			dstMesh.meshlets[i].vertexOffset = meshlets[i].vertex_offset;
-			dstMesh.meshlets[i].vertexCount = meshlets[i].vertex_count;
-			dstMesh.meshlets[i].primitiveOffset = meshlets[i].triangle_offset;
-			dstMesh.meshlets[i].primitiveCount = meshlets[i].triangle_count;
-			primitiveCount += dstMesh.meshlets[i].primitiveCount;
-		}
-
-		// プリミティブインデックスをパックする
-		dstMesh.packedPrimitiveIndices.resize(primitiveCount);
-		for (size_t count = 0; auto & i : dstMesh.packedPrimitiveIndices) {
-			i = static_cast<uint32_t>(dstMesh.primitiveIndices[count + 0]);
-			i <<= 8;
-			i += static_cast<uint32_t>(dstMesh.primitiveIndices[count + 1]);
-			i <<= 8;
-			i += static_cast<uint32_t>(dstMesh.primitiveIndices[count + 2]);
-
-			count += 3;
-		}
-
-		dstMesh.meshlets.shrink_to_fit();
+	// 失敗したらthrowする
+	if (FAILED(hr)) {
+		throw Lamb::Error::Code<MeshLoader>("DirectX::ComputeMeshlets failed", ErrorPlace);
 	}
 
+	// データを設定
+	dstMesh.meshlets = std::move(meshlets);
 
+	dstMesh.uniqueVertexIndices.resize(uniqueVertexIB.size());
+	std::memcpy(dstMesh.uniqueVertexIndices.data(), uniqueVertexIB.data(), sizeof(uint8_t) * uniqueVertexIB.size());
+
+	dstMesh.primitiveIndices.resize(primitiveIndices.size());
+	for (size_t count = 0; auto& primitiveIndex : primitiveIndices) {
+		dstMesh.primitiveIndices[count] = primitiveIndex.i0;
+		dstMesh.primitiveIndices[count] <<= 10;
+		dstMesh.primitiveIndices[count] += primitiveIndex.i1;
+		dstMesh.primitiveIndices[count] <<= 10;
+		dstMesh.primitiveIndices[count] += primitiveIndex.i2;
+
+
+		count++;
+	}
+
+	std::memcpy(dstMesh.primitiveIndices.data(), primitiveIndices.data(), sizeof(DirectX::MeshletTriangle) * primitiveIndices.size());
 }
 
 std::unique_ptr<MeshletManager> MeshletManager::instance_;
@@ -216,28 +123,13 @@ void MeshletManager::LoadMesh(const std::string& fileName) {
 
 		shaderData->gVertices.Create(static_cast<uint32_t>(resMesh->vertices.size()));
 		shaderData->gUniqueVertexIndices.Create(static_cast<uint32_t>(resMesh->uniqueVertexIndices.size()));
-		shaderData->gUniquePrimitiveIndices.Create(static_cast<uint32_t>(resMesh->packedPrimitiveIndices.size()));
+		shaderData->gPrimitiveIndices.Create(static_cast<uint32_t>(resMesh->primitiveIndices.size()));
 		shaderData->gMeshlets.Create(static_cast<uint32_t>(resMesh->meshlets.size()));
 
-		for (size_t count = 0; auto & vertex : resMesh->vertices) {
-			shaderData->gVertices[count] = vertex;
-			count++;
-		}
-
-		for (size_t count = 0; auto & index : resMesh->uniqueVertexIndices) {
-			shaderData->gUniqueVertexIndices[count] = index;
-			count++;
-		}
-
-		for (size_t count = 0; auto & primitiveIndex : resMesh->packedPrimitiveIndices) {
-			shaderData->gUniquePrimitiveIndices[count] = primitiveIndex;
-			count++;
-		}
-
-		for (size_t count = 0; auto & meshlet : resMesh->meshlets) {
-			shaderData->gMeshlets[count] = meshlet;
-			count++;
-		}
+		shaderData->gVertices.MemCpy(resMesh->vertices.data(), sizeof(Vertex) * resMesh->vertices.size());
+		shaderData->gUniqueVertexIndices.MemCpy(resMesh->uniqueVertexIndices.data(), sizeof(uint32_t) * resMesh->uniqueVertexIndices.size());
+		shaderData->gPrimitiveIndices.MemCpy(resMesh->primitiveIndices.data(), sizeof(uint32_t) * resMesh->primitiveIndices.size());
+		shaderData->gMeshlets.MemCpy(resMesh->meshlets.data(), sizeof(DirectX::Meshlet) * resMesh->meshlets.size());
 
 		shaderData->meshletCount = static_cast<uint32_t>(shaderData->gMeshlets.size());
 
@@ -246,7 +138,7 @@ void MeshletManager::LoadMesh(const std::string& fileName) {
 		descriptorHeap->BookingHeapPos(5);
 		descriptorHeap->CreateView(shaderData->gVertices);
 		descriptorHeap->CreateView(shaderData->gUniqueVertexIndices);
-		descriptorHeap->CreateView(shaderData->gUniquePrimitiveIndices);
+		descriptorHeap->CreateView(shaderData->gPrimitiveIndices);
 		descriptorHeap->CreateView(shaderData->gMeshlets);
 		descriptorHeap->CreateView(shaderData->gTransform);
 
@@ -254,7 +146,7 @@ void MeshletManager::LoadMesh(const std::string& fileName) {
 
 		shaderData->gVertices.OffWright();
 		shaderData->gUniqueVertexIndices.OffWright();
-		shaderData->gUniquePrimitiveIndices.OffWright();
+		shaderData->gPrimitiveIndices.OffWright();
 		shaderData->gMeshlets.OffWright();
 		shaderData->gTransform.OffWright();
 	}
